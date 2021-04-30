81feb19c114034d3ba228b2a6825b8dd284b27fa
Sarthak Sharma <sarthaksharma_ma@srmuniv.edu.in>
2021-04-30 14:50:50 +0530

    basic evernote

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+
+// const firebase= require('firebase');
+// require('firebase/firestore');
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBzESktWYgD-sDUVEIdmAs7GwBtjmvULgk",
+  authDomain: "evernote-c2501.firebaseapp.com",
+  projectId: "evernote-c2501",
+  storageBucket: "evernote-c2501.appspot.com",
+  messagingSenderId: "769377414737",
+  appId: "1:769377414737:web:e8cbf67bac1a9f61159270"
+});
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/sidebaritem/sidebarItem.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { removeHTMLTags } from '../helpers';
+
+class SidebarItemComponent extends React.Component{
+
+    constructor(){
+
+        super();
+    }
+
+    render(){
+
+        const {classes,_note,_index,selectedNodeIndex} = this.props;
+
+
+        return(
+            <div key={_index}>
+                <ListItem
+                  className={classes.listItem}
+                  selected={selectedNodeIndex===_index}
+                  alignItems="flex-start"
+                >
+                    <div className={classes.textSection}
+                       onClick={()=>this.selectNote(_note,_index)}>
+                           <ListItemText
+                             primary={_note.title}
+                             secondary={removeHTMLTags(_note.body.substring(0,30))+'...'}
+                             >
+
+                             </ListItemText>
+                    </div>
+                    <DeleteIcon onClick={()=> this.deleteNote(_note)}
+                        className={classes.deleteIcon}></DeleteIcon>
+                </ListItem>
+            </div>
+        
+        )
+    }
+
+    selectNote = (n,i) => this.props.selectNote(n,i);
+    deleteNote = (note) => {
+        if(window.confirm(`Are you sure you want to delete this: ${note.title}`)){
+            this.props.deleteNote(note);
+        }
+    }
+
+}
+
+export default withStyles(styles)(SidebarItemComponent)
